34facee31e4e87cc7a3b77787cd4128b45edd843
lllllllqw <465633856@qq.com>
2023-04-16 23:16:37 +0800

    refator: vscode version update

--- src/extension.ts
+++ src/extension.ts
@@ -1,13 +1,13 @@
-import * as vscode from 'vscode'
-import { genJSDoc } from './main'
+import * as vscode from 'vscode';
+import { genJSDoc } from './main';
 
 export function activate(context: vscode.ExtensionContext) {
   const disposable = vscode.commands.registerCommand(
     'extension.genJSDoc',
-    genJSDoc
-  )
+    genJSDoc,
+  );
 
-  context.subscriptions.push(disposable)
+  context.subscriptions.push(disposable);
 }
 
 export function deactivate() {}
